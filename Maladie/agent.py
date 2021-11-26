edc3326b32d7c31bdfc45bea37240f8e8645c109
YamatoFr <60272310+YamatoFr@users.noreply.github.com>
2021-11-26 00:05:23 -0400

    added infecter, TODO guerison, other

--- Maladie/agent.py
+++ Maladie/agent.py
@@ -1,7 +1,10 @@
-import random
+from random import randrange, random
 
 TAILLE_MONDE = 5000
-
+PROB_INF = 0.1 # probabilité d'infection MAX = 1
+DIST_INF = 10 # distance d'infection 
+PROB_REC = 0.1 # probabilité de guérison MAX = 1
+states = ["healthy", "infected", "immune", "vaccinated"]
 
 class Agent:
     def __init__(self, state, x, y, vitesse):
@@ -10,10 +13,23 @@ class Agent:
         self.y = y
         self.vitesse = 20
 
+    # l'agent se déplace
     def deplacement(self):
         self.x += (self.vitesse//2 -
-                   random.randrange(-self.vitesse, self.vitesse))
+                   randrange(-self.vitesse, self.vitesse))
         self.y += (self.vitesse//2 -
-                   random.randrange(-self.vitesse, self.vitesse))
+                   randrange(-self.vitesse, self.vitesse))
         self.x %= TAILLE_MONDE
         self.y %= TAILLE_MONDE
+
+    # un agent infecté contaminera un autre agent si il est à une distance DIST_INF
+    # et selon la probabilité PROB_INF
+    def infecter(self, other):
+        if (self.state == states[1] and other.state == states[0] and
+            random() < PROB_INF):
+            other.state = states[1]
+    
+    # un agent infecté sera guéri après un certain temps (entre 14 et 28 itérations)
+    # selon la probabilité PROB_REC, il devient donc immunisé pendant 28 itérations
+    # def guerison(self):
+        # TODO: à compléter
--- Maladie/main.py
+++ Maladie/main.py
@@ -2,28 +2,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+# from agent import Agent, states, TAILLE_MONDE
+import agent
 
-NB_ITER = 1095
-PROB_INF = 0.1
-DIST_INF = 10
-PROB_REC = 0.1
-
-states = ["healthy", "infected", "immune", "vaccinated"]
+NB_ITER = 1095 # Nombre d'itérations (jours)
+PROB_INF = 0.1 # probabilité d'infection
+PROB_REC = 0.1 # probabilité de guérison
 
 # on entre la population initiale
 print("Entrez la population initiale : ")
 nb_agents = int(input())
 
+# les agents sont créés
+agents = [agent.Agent(nb_agents) for i in range(nb_agents)]
+
 # on le nombre d'infectés initial
 print("Entrez le nombre d'infectés initial : ")
 nb_infectes = int(input())
 
-# boucle principale
-for t in range(NB_ITER):
-    # on initialise les tableaux de population
-    population = np.zeros(nb_agents, dtype=int)
-    population_infectes = np.zeros(nb_agents, dtype=int)
+# on infecte les premiers agents
+for i in range(nb_infectes):
+    agents[i].state = agent.states[1]
 
-    # on initialise les tableaux de probabilités
-    proba_infection = np.zeros(nb_agents, dtype=float)
-    proba_recovery = np.zeros(nb_agents, dtype=float)
+# boucle principale
+for i in range(NB_ITER):
+    # les agents se déplacent et infectent les autres selon les probabilités définies
+    for agent in agents:
+        agent.deplacement()
+        agent.infecte()
+        agent.gueris()
