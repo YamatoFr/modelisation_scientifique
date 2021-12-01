a5d79999544bb07cfcb384eee0058fb3e610a91c
YamatoFr <60272310+YamatoFr@users.noreply.github.com>
2021-12-01 00:19:09 -0400

    see changelog

--- Modele_de_Schelling/README.md
+++ Modele_de_Schelling/README.md
@@ -1,5 +1,50 @@
 <center><h1> Modèle de Schelling </h1></center>
 
+## Sommaire
+1. [Introduction](#introduction)
+2. [Présentation du modèle](#presenration-du-modele)  
+   a. [Définition des paramètres](#definition-des-parametres)  
+   b. [Présentation des individus](#presentation-des-individus)  
+3. [Expériances éffectuées](#experiences-effectuees)
+
+&nbsp;  
+
+# Introduction  <a name="introduction"></a>
+
+<div style="text-align: justify">
 Cette simulation a pour but de montrer comment le modèle de Schelling peut être utilisé pour simuler un système de répartition d'une population mobile.
 Nous étudirons la façon dont les individus de répartissent dans un espace définis et durant les expériences, nous ferons varier plusieurs paramètres
-afin d'observer leur impact sur la population.
+afin d'observer leur impact sur la population. Notre problématique est la suivante : quel facteur influence la répartition des individus dans un espace défini ?
+Pour cela nous présenterons le modèle utilisé, les expériences menées et les résultats obtenus.
+</div>
+
+## Présentation du modèle  <a name="presenration-du-modele"></a>
+
+### Définition des paramètres  <a name="definition-des-parametres"></a>
+
+<div style="text-align: justify">
+Pour ce modèle, les paramètres que nous allons utiliser sont les suivants :
+<ul>
+    <li> la durée de la simulation : un maximum de 10 000 itérations sera effectué, la simulation se terminera avant si la population est stable.
+    <li> la taille du monde dans lequel les individus vont se déplacer.
+    <li> le taux d'insatisfaction des individus.
+    <li> la densité de la population.
+</ul>
+</div>
+
+### Présentation des individus  <a name="presentation-des-individus"></a>
+
+<div style="text-align: justify">
+Les individus sont représentés par des carrés colorés de taille 1x1, les carrés jaunes et violets représentent chacun un "type" d'individu et les carrés turquoises 
+représentent las cases vides. L'insatisfaction des individus est déterminé par leur voisinage, c'est-à-dire les cases adjacentes à l'individu (cela 
+inclut également les diagonales), et le taux d'insatisfaction définis. Ils se déplacent dans un monde dont la taille est définie au début de l'expérience, 
+les individus chercheront à se déplacer de façon à être satisfait de leur voisinage.
+</div>
+
+<div style="page-break-after: always;"></div>
+
+## Expériences éffectuées  <a name="experiences-effectuees"></a>  
+
+<div style="text-align: justify">
+
+</div>
--- Modele_de_Schelling/schelling.py
+++ Modele_de_Schelling/schelling.py
@@ -1,80 +1,120 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from time import time
 
-SIZE = 20
-TAUX_INS = 0.2
-TIME = 10000
-DENS = ((SIZE*SIZE)//2) - 20 
 
-def insat(world,i,j):
+def insat(world, i, j, size):
     u = 0
-    
-    if world[(i+1)%SIZE,(j+1)%SIZE] !=0 and  world[(i+1)%SIZE,(j+1)%SIZE] != world[i,j] : u+=1
-    if world[(i+1)%SIZE,(j  )%SIZE] !=0 and  world[(i+1)%SIZE,(j  )%SIZE] != world[i,j] : u+=1
-    if world[(i+1)%SIZE,(j-1)%SIZE] !=0 and  world[(i+1)%SIZE,(j-1)%SIZE] != world[i,j] : u+=1
-    if world[(i  )%SIZE,(j+1)%SIZE] !=0 and  world[(i  )%SIZE,(j+1)%SIZE] != world[i,j] : u+=1
-    if world[(i  )%SIZE,(j-1)%SIZE] !=0 and  world[(i  )%SIZE,(j-1)%SIZE] != world[i,j] : u+=1
-    if world[(i-1)%SIZE,(j+1)%SIZE] !=0 and  world[(i-1)%SIZE,(j+1)%SIZE] != world[i,j] : u+=1
-    if world[(i-1)%SIZE,(j  )%SIZE] !=0 and  world[(i-1)%SIZE,(j  )%SIZE] != world[i,j] : u+=1
-    if world[(i-1)%SIZE,(j-1)%SIZE] !=0 and  world[(i-1)%SIZE,(j-1)%SIZE] != world[i,j] : u+=1
 
-    return u/8;   
+    if world[(i+1) % size, (j+1) % size] != 0 and world[(i+1) % size, (j+1) % size] != world[i, j]:
+        u += 1
+    if world[(i+1) % size, (j) % size] != 0 and world[(i+1) % size, (j) % size] != world[i, j]:
+        u += 1
+    if world[(i+1) % size, (j-1) % size] != 0 and world[(i+1) % size, (j-1) % size] != world[i, j]:
+        u += 1
+    if world[(i) % size, (j+1) % size] != 0 and world[(i) % size, (j+1) % size] != world[i, j]:
+        u += 1
+    if world[(i) % size, (j-1) % size] != 0 and world[(i) % size, (j-1) % size] != world[i, j]:
+        u += 1
+    if world[(i-1) % size, (j+1) % size] != 0 and world[(i-1) % size, (j+1) % size] != world[i, j]:
+        u += 1
+    if world[(i-1) % size, (j) % size] != 0 and world[(i-1) % size, (j) % size] != world[i, j]:
+        u += 1
+    if world[(i-1) % size, (j-1) % size] != 0 and world[(i-1) % size, (j-1) % size] != world[i, j]:
+        u += 1
 
-def getOpen(world):
+    return u/8
+
+
+def getOpen(world, size):
     L = []
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if world[i][j] == 0 :
-                L.append((i,j))
+    for i in range(size):
+        for j in range(size):
+            if world[i][j] == 0:
+                L.append((i, j))
     return L
 
-def getInsat(world):
+
+def getInsat(world, size, taux_ins):
     L = []
-    for i in range(SIZE):
-        for j in range(SIZE):
-            if world[i,j] != 0 and insat(world,i,j) > TAUX_INS :
-                L.append((i,j))
+    for i in range(size):
+        for j in range(size):
+            if world[i, j] != 0 and insat(world, i, j, size) > taux_ins:
+                L.append((i, j))
     return L
 
 
-world = np.array([[0]*SIZE]*SIZE)
+def main(size, TIME, taux_ins):
+    dens = ((size*size)//2) - 20
+    world = np.array([[0]*size]*size)
 
+    i = 0
+    while i < dens:
+        x = random.randrange(size)
+        y = random.randrange(size)
+        if world[x, y] == 0:
+            world[x, y] = 1
+            i += 1
 
-i=0
-while i<DENS:
-    x = random.randrange(SIZE)
-    y = random.randrange(SIZE)
-    if world[x,y] == 0 :
-        world[x,y] = 1
+    i = 0
+    while i < dens:
+        x = random.randrange(size)
+        y = random.randrange(size)
+        if world[x, y] == 0:
+            world[x, y] = -1
+            i += 1
+
+    plt.imshow(world, interpolation='none')
+    plt.title("Répartition de base")
+    plt.savefig("./Images/" + "t" + str(size) + "tins" +
+                str(taux_ins) + "dens" + str(abs(dens - ((size*size)//2))) + ".png")
+    plt.close()
+
+    for i in range(TIME):
+        insatL = list(getInsat(world, size, taux_ins))
+        if len(insatL) > 0:
+            x1, y1 = random.choice(insatL)
+            x2, y2 = random.choice(getOpen(world, size))
+
+            world[x2, y2] = world[x1, y1]
+            world[x1, y1] = 0
+        else:
+            print('break', i)
+            break
+
+    if i == TIME - 1:
         i += 1
 
-i=0
-while i<DENS:
-    x = random.randrange(SIZE)
-    y = random.randrange(SIZE)
-    if world[x,y] == 0 :
-        world[x,y] = -1
-        i += 1
+    plt.imshow(world, interpolation='none')
+    plt.title("Répartition après " + str(i) + " itérations")
+    plt.savefig("./Images/" + "t" + str(size) + "tins" +
+                str(taux_ins) + "dens" + str(abs(dens - ((size*size)//2))) + "fin.png")
+    plt.close()
 
-plt.imshow(world, interpolation='none')
-plt.title("Répartition de base")
-plt.savefig("./Images/base.png")
-plt.close()
 
-for i in range(TIME):
-    insatL = list(getInsat(world))
-    if len(insatL) > 0 :
-        x1,y1 = random.choice(insatL)
-        x2,y2 = random.choice(getOpen(world))
+# size : taille du tableau
+# taux_ins : taux d'insatisfaction
+TIME = 10000
 
-        world[x2,y2] = world[x1,y1]
-        world[x1,y1] = 0
-    else : 
-        print('break',i)
-        break
+start_time = time()
 
-plt.imshow(world, interpolation='none')
-plt.title("Répartition après " + str(TIME) + " itérations")
-plt.savefig("./Images/fin.png")
-plt.close()
+main(10, TIME, 0.1)
+main(10, TIME, 0.25)
+main(10, TIME, 0.5)
+main(10, TIME, 0.75)
+main(10, TIME, 1.0)
+
+main(20, TIME, 0.1)
+main(20, TIME, 0.25)
+main(20, TIME, 0.5)
+main(20, TIME, 0.75)
+main(20, TIME, 1.0)
+
+main(50, TIME, 0.1)
+main(50, TIME, 0.25)
+main(50, TIME, 0.5)
+main(50, TIME, 0.75)
+main(50, TIME, 1.0)
+
+print("--- %s seconds ---" % (time() - start_time))
